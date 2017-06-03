8ad78ed3c1e76b6be7d39b5595ede1c4604e2863
javademami <aliabadi7697@gmail.com>
2017-06-03 14:32:54 +0430

    Menu created

--- src/StrongholdCrusader/Menu.java
+++ src/StrongholdCrusader/Menu.java
@@ -1,7 +1,19 @@
 package StrongholdCrusader;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 /**
  * Created by MiladIbra on 6/3/2017.
  */
-public class Menu {
+public class Menu extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setScene(new Scene
+                ( FXMLLoader.load(getClass().getResource("/ui/MenuBG.fxml"))));
+        stage.show();
+    }
 }
--- src/StrongholdCrusader/MenuGUI.java
+++ src/StrongholdCrusader/MenuGUI.java
@@ -1,7 +1,51 @@
 package StrongholdCrusader;
 
+import StrongholdCrusader.Network.Server;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
 /**
  * Created by MiladIbra on 6/3/2017.
  */
-public class MenuGUI {
+public class MenuGUI implements Initializable {
+
+    @FXML
+    ImageView image;
+    @FXML
+    Button server;
+    @FXML
+    Button join;
+    @FXML
+    Button exit;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        server.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                Server server1 = new Server();
+            }
+        });
+        join.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                //???
+            }
+        });
+        exit.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                System.exit(0);
+            }
+        });
+    }
 }
