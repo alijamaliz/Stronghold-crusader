fa567835e53ff6d3c1f7d84709365da0ba9708d8
Unknown <miladibra10@gmail.com>
2017-06-03 12:51:58 +0430

    Some functions of Resource Manager added
    functions of images for Buildings & Humans

--- src/StrongholdCrusader/ResourceManager.java
+++ src/StrongholdCrusader/ResourceManager.java
@@ -1,7 +1,75 @@
 package StrongholdCrusader;
 
+import javafx.scene.image.Image;
+
+import java.io.File;
+
 /**
  * Created by Baran on 5/29/2017.
  */
 public class ResourceManager {
+    public Image getBuilding(String name)
+    {
+        switch (name)
+        {
+            case "Barracks" : {
+                File tmp = new File("../Resources/images/Buildings/Barracks.png");
+                Image image = new Image(tmp.toURI().toString());
+                return image;
+            }
+            case "Farm" : {
+                File tmp = new File("../Resources/images/Buildings/Farm.png");
+                Image image = new Image(tmp.toURI().toString());
+                return image;
+            }
+            case "Market" : {
+                File tmp = new File("../Resources/images/Buildings/Market.png");
+                Image image = new Image(tmp.toURI().toString());
+                return image;
+            }
+            case "Palace" : {
+                File tmp = new File("../Resources/images/Buildings/Palace.png");
+                Image image = new Image(tmp.toURI().toString());
+                return image;
+            }
+            case "Port" : {
+                File tmp = new File("../Resources/images/Buildings/Port.png");
+                Image image = new Image(tmp.toURI().toString());
+                return image;
+            }
+            case "Quarry" : {
+                File tmp = new File("../Resources/images/Buildings/Barracks.png");
+                Image image = new Image(tmp.toURI().toString());
+                return image;
+            }
+            case "WoodCutter" : {
+                File tmp = new File("../Resources/images/Buildings/WoodCutter.png");
+                Image image = new Image(tmp.toURI().toString());
+                return image;
+            }
+            default: return null;
+        }
+    }
+    public Image getHuman(String name)
+    {
+        switch (name)
+        {
+            case "Soldier" : {
+                File tmp = new File("../Resources/images/Humans/Soldier.png");
+                Image image = new Image(tmp.toURI().toString());
+                return image;
+            }
+            case "Worker" : {
+                File tmp = new File("../Resources/images/Humans/Worker.png");
+                Image image = new Image(tmp.toURI().toString());
+                return image;
+            }
+            case "Vassal" : {
+                File tmp = new File("../Resources/images/Humas/Vassal.png");
+                Image image = new Image(tmp.toURI().toString());
+                return image;
+            }
+            default: return null;
+        }
+    }
 }
