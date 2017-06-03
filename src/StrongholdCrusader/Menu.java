7f701c586281a513321bc1fa0295d584a91bfd96
javademami <aliabadi7697@gmail.com>
2017-06-03 15:16:19 +0430

    Menu problem resolved

--- src/StrongholdCrusader/Menu.java
+++ src/StrongholdCrusader/Menu.java
@@ -2,18 +2,28 @@ package StrongholdCrusader;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
 /**
  * Created by MiladIbra on 6/3/2017.
  */
 public class Menu extends Application {
     @Override
-    public void start(Stage stage) throws Exception {
-        stage.setScene(new Scene
-                ( FXMLLoader.load(getClass().getResource("/ui/MenuBG.fxml"))));
+    public void start(Stage stage)  {
+        stage.setTitle("Menu");
+        try {
+
+            stage.setScene(new Scene
+                    ( FXMLLoader.load(getClass().getResource("menu.fxml"))));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         stage.show();
     }
 }
