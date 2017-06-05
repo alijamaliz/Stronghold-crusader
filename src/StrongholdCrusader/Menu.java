1af9d276ba33a1170e4f0679a210dd40f5ef01b0
javademami <aliabadi7697@gmail.com>
2017-06-05 17:44:24 +0430

    in developing menu

--- src/StrongholdCrusader/Menu.java
+++ src/StrongholdCrusader/Menu.java
@@ -2,6 +2,7 @@ package StrongholdCrusader;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -12,18 +13,32 @@ import java.io.IOException;
  */
 public class Menu extends Application {
     public static Stage stage;
+    public Stage window;
+    public Parent root;
+    public static int a = 0;
     @Override
-    public void start(Stage primaryStage)  {
-        Menu.stage = primaryStage;
+    public void start(Stage stage)  {
+        window = stage;
+
         stage.setTitle("Menu");
         try {
-            Menu.stage.setScene(new Scene
-                    ( FXMLLoader.load(getClass().getResource("ui/menu.fxml"))));
-
-
+                root = FXMLLoader.load(getClass().getResource("ui/menu.fxml"));
         } catch (IOException e) {
             e.printStackTrace();
         }
-        stage.show();
+        window.setScene(new Scene(root));
+        window.show();
+        if (a == 1){
+            try {
+                root = FXMLLoader.load(getClass().getResource("ui/join.fxml"));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            window.setScene(new Scene(root));
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
--- src/StrongholdCrusader/MenuGUI.java
+++ src/StrongholdCrusader/MenuGUI.java
@@ -1,15 +1,22 @@
 package StrongholdCrusader;
 
 import StrongholdCrusader.Network.Server;
+import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 
 import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -18,14 +25,17 @@ import java.util.ResourceBundle;
  */
 public class MenuGUI implements Initializable {
 
+    //mainmenu
     @FXML
     ImageView image;
     @FXML
     Button server;
     @FXML
-    Button join;
+    Button joinpage;
     @FXML
     Button exit;
+
+
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -33,16 +43,17 @@ public class MenuGUI implements Initializable {
         Image image1 = new Image(file.toURI().toString());
         image.setImage(image1);
 
+
         server.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
                 Server server1 = new Server();
             }
         });
-        join.setOnAction(new EventHandler<ActionEvent>() {
+        joinpage.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
-                ClientPlayer clientPlayer = new ClientPlayer("Ali");
+                Menu.a = 1;
             }
         });
         exit.setOnAction(new EventHandler<ActionEvent>() {
@@ -51,5 +62,7 @@ public class MenuGUI implements Initializable {
                 System.exit(0);
             }
         });
+
+
     }
 }
