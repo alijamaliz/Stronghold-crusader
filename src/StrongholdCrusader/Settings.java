57bdfb3861a45388a5a51a6e1963619a74435af3
javademami <aliabadi7697@gmail.com>
2017-06-16 16:08:09 +0430

    basic zooming on map implemented

--- src/StrongholdCrusader/Settings.java
+++ src/StrongholdCrusader/Settings.java
@@ -7,11 +7,11 @@ public class Settings {
     public static final int SERVER_PORT = 7777;
     public static final int FRAME_RATE = 20;
     public static final int PACKET_MAX_SIZE = 65536;
-    public static final int MAP_WIDTH_RESOLUTION = 100;
-    public static final int MAP_HEIGHT_RESOLUTION = 100;
-    public static final int MAP_NAVIGATION_SPEED = 50;
+    public static  int MAP_WIDTH_RESOLUTION = 100;
+    public static  int MAP_HEIGHT_RESOLUTION = 100;
+    public static int MAP_NAVIGATION_SPEED = 50;
     public static final int MOUSE_MAP_NAVIGATION_MARGIN = 10;
-    public static final int MAP_TILES_WIDTH = 30;
-    public static final int MAP_TILES_HEIGHT = 30;
+    public static int MAP_TILES_WIDTH = 30;
+    public static int MAP_TILES_HEIGHT = 30;
 
 }
