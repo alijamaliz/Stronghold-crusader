39183425d77c5ee6028ecb0f5de769e074673882
Unknown <jamaliali219@gmail.com>
2017-06-17 07:11:04 +0430

    Cursor image implemented

--- src/StrongholdCrusader/ResourceManager.java
+++ src/StrongholdCrusader/ResourceManager.java
@@ -1,6 +1,8 @@
 package StrongholdCrusader;
 
 import StrongholdCrusader.GameObjects.Buildings.Barracks;
+import javafx.scene.Cursor;
+import javafx.scene.ImageCursor;
 import javafx.scene.image.Image;
 
 import java.io.File;
@@ -76,68 +78,9 @@ public class ResourceManager {
         }
         else return null;
     }
-    /*public Image getBuilding(String name)
-    {
-        switch (name)
-        {
-            case "Barracks" : {
-                File tmp = new File("Resources/images/Buildings/Barracks.png");
-                Image image = new Image(tmp.toURI().toString());
-                return image;
-            }
-            case "Farm" : {
-                File tmp = new File("Resources/images/Buildings/Farm.png");
-                Image image = new Image(tmp.toURI().toString());
-                return image;
-            }
-            case "Market" : {
-                File tmp = new File("Resources/images/Buildings/Market.png");
-                Image image = new Image(tmp.toURI().toString());
-                return image;
-            }
-            case "Palace" : {
-                File tmp = new File("Resources/images/Buildings/Palace.png");
-                Image image = new Image(tmp.toURI().toString());
-                return image;
-            }
-            case "Port" : {
-                File tmp = new File("Resources/images/Buildings/Port.png");
-                Image image = new Image(tmp.toURI().toString());
-                return image;
-            }
-            case "Quarry" : {
-                File tmp = new File("Resources/images/Buildings/Barracks.png");
-                Image image = new Image(tmp.toURI().toString());
-                return image;
-            }
-            case "WoodCutter" : {
-                File tmp = new File("Resources/images/Buildings/WoodCutter.png");
-                Image image = new Image(tmp.toURI().toString());
-                return image;
-            }
-            default: return null;
-        }
+    public Cursor getCursor(String name) {
+        Image image = getImage(name);
+        ImageCursor imageCursor = new ImageCursor(image);
+        return imageCursor;
     }
-    public Image getHuman(String name)
-    {
-        switch (name)
-        {
-            case "Soldier" : {
-                File tmp = new File("Resources/images/Humans/Soldier.png");
-                Image image = new Image(tmp.toURI().toString());
-                return image;
-            }
-            case "Worker" : {
-                File tmp = new File("Resources/images/Humans/Worker.png");
-                Image image = new Image(tmp.toURI().toString());
-                return image;
-            }
-            case "Vassal" : {
-                File tmp = new File("Resources/images/Humas/Vassal.png");
-                Image image = new Image(tmp.toURI().toString());
-                return image;
-            }
-            default: return null;
-        }
-    }*/
 }
